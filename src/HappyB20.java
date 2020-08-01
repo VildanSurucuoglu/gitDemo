0543d9fd2dcca9927919f3898ce744aaba3ff353
VildanSurucuoglugit config --global user.email vildanorhan10@gmail.comgit config --global user.password cih10vil10 <vildanorhan@gmail.com>
2020-08-01 12:27:29 -0400

    Happpy Batch20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,6 +3,7 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("B20 is always happy");
+        System.out.println("B20 is always happy, anyway");
 
 
 
